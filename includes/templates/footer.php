ed507e845c260dfc19f9703fe894347985ea8c8b
irfandwisamudra <irfandwisamudra08@gmail.com>
2023-12-08 14:51:30 +0700

    Change footer

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,44 +1,50 @@
-<div class="container">
-  <footer class="row row-cols-2 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
-    <div class="col col-md-5 me-md-auto mb-3">
-      <h5>About Us</h5>
-      <p class="text-body-secondary">Short description about your website or company.</p>
-    </div>
+<footer class="border-top">
+  <div class="container">
+    <div class="row row-cols-1 row-cols-md-5 py-5">
+      <div class="col col-md-5 me-md-auto mb-3">
+        <h5>About Us</h5>
+        <p class="text-body-secondary">Short description about your website or company.</p>
+      </div>
 
-    <div class="col col-md-2 mb-3">
-      <h5>Quick Links</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Recipes</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Category</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Gallery</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Contact</a></li>
-      </ul>
-    </div>
+      <div class="col col-md-2 mb-3">
+        <h5>Quick Links</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Recipes</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Category</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Gallery</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Contact</a></li>
+        </ul>
+      </div>
 
-    <div class="col col-md-2 mb-3">
-      <h5>Pages</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Privacy Policy</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Terms of Service</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-      </ul>
-    </div>
+      <div class="col col-md-2 mb-3">
+        <h5>Pages</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Privacy Policy</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Terms of Service</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+        </ul>
+      </div>
 
-    <div class="col col-md-3 mb-3">
-      <h5>Contact Us</h5>
-      <p class="text-body-secondary">Address: 123 Main Street, City</p>
-      <p class="text-body-secondary">Phone: +123 456 789</p>
-      <p class="text-body-secondary">Email: info@example.com</p>
+      <div class="col col-md-3 mb-3">
+        <h5>Contact Us</h5>
+        <p class="text-body-secondary">Address: Jalan Trunojoyo, Bangkalan</p>
+        <p class="text-body-secondary">Phone: +62 852 3655 3899</p>
+        <p class="text-body-secondary">Email: empiregalaxyyy@gmail.com</p>
+      </div>
     </div>
+  </div>
 
-    <div class="col col-md-12 d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-      <p>&copy; 2023 Company, Inc. All rights reserved.</p>
-      <ul class="list-unstyled d-flex">
-        <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-twitter" style="font-size: 24px;"></i></a></li>
-        <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-instagram" style="font-size: 24px;"></i></a></li>
-        <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-facebook" style="font-size: 24px;"></i></a></li>
-      </ul>
+  <div class="copyright border-top">
+    <div class="container">
+      <div class="col col-md-12 d-flex flex-column flex-sm-row justify-content-between py-4">
+        <p class="text-light">&copy; 2023 TasteTrekker. All rights reserved.</p>
+        <ul class="list-unstyled d-flex">
+          <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-twitter" style="font-size: 24px;"></i></a></li>
+          <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-instagram" style="font-size: 24px;"></i></a></li>
+          <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-facebook" style="font-size: 24px;"></i></a></li>
+        </ul>
+      </div>
     </div>
-  </footer>
-</div>
+  </div>
+</footer>
