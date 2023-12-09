4909d10db7ab2e01dec0f943eab93c10b44c21e2
irfandwisamudra <irfandwisamudra08@gmail.com>
2023-12-09 17:24:12 +0700

    Change file index homepage

--- includes/database.php
+++ includes/database.php
@@ -1,2 +1,10 @@
 <?php
 require_once("base.php");
+
+function getAllDataMenus() {
+  return mysqli_query($GLOBALS["db"],"SELECT * FROM menu")->fetch_all(MYSQLI_ASSOC);
+}
+
+function getAllDataCategories() {
+  return mysqli_query($GLOBALS["db"],"SELECT * FROM category")->fetch_all(MYSQLI_ASSOC);
+}
