75d69b675d79feae41e3b1968953f6d1277ea240
NurulRizka <nurulrizka222@gmail.com>
2023-12-09 19:59:04 +0700

    Change database.php

--- includes/base.php
+++ includes/base.php
@@ -1,6 +1,6 @@
 <?php
 ob_start();
 session_start();
-define("BASEURL", "http://localhost/TasteTrekker");
+define("BASEURL", "http://localhost:8080/TasteTrekker");
 define("BASEPATH", $_SERVER["DOCUMENT_ROOT"] . "/TasteTrekker");
 $GLOBALS["db"] = mysqli_connect("localhost", "root", "", "tastetrekker");
--- includes/database.php
+++ includes/database.php
@@ -1,2 +1,8 @@
 <?php
 require_once("base.php");
+
+
+function getAllDataCategory()
+{
+    return mysqli_query($GLOBALS["db"], "SELECT * FROM category")->fetch_all(MYSQLI_ASSOC);
+}
