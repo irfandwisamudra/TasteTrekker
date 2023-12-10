b7fc19e71fd0eb9aec19ae89670a583a83c43c31
NurulRizka <nurulrizka222@gmail.com>
2023-12-10 19:59:31 +0700

    Change category.php

--- category.php
+++ category.php
@@ -8,8 +8,16 @@ include "./includes/main_start.php";
 <?php
 $categories = getAllCategories();
 ?>
+<div class="container">
+  <div class="row mt-5">
+  </div>
+  <div class="row mt-5">
+    <div class="col">
+      <h2 class="fw-bold text-center">KATEGORI MENU</h2>
+    </div>
+  </div>
+</div>
 
-<H2 class="fw-bold text-center py-5">KATEGORI MENU</H2>
 
 <!-- Carousel wrapper -->
 <div id="carouselMultiItemExample" class="carousel slide carousel-dark text-center" data-mdb-ride="carousel">
@@ -22,8 +30,7 @@ $categories = getAllCategories();
           <?php foreach ($categories as $category) : ?>
             <div class="col-lg-4 mb-5">
               <div class="card">
-                <img src="<?= BASEURL ?>/assets/img/<?= $category["image_category"]; ?>" class="card-img-top img-fluid" alt="<?= ucwords($category["name_category"]); ?>" style="height: 300px; object-fit: cover;">
-                <img src="assets/img/category/<?= $category['image_category']; ?>" class="card-img-top img-fluid" alt="Waterfall" style="height: 300px; object-fit: cover;">
+                <img src="assets/img/category/<?= $category['image_category']; ?>" class="card-img-top img-fluid" alt="Waterfall" style="height: 300px; object-fit: cover;" alt="<?= ucwords($category["name_category"]); ?>">
                 <div class="card-body">
                   <h5 class="card-title"><?= $category["name_category"]; ?></h5>
                   <p class="card-text">
