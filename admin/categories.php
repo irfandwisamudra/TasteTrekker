0968d53e5151a6cb4915f44d73fd8c92043b7d40
irfandwisamudra <irfandwisamudra08@gmail.com>
2023-12-26 11:34:27 +0700

    Delete text in span

--- admin/categories.php
+++ admin/categories.php
@@ -12,7 +12,6 @@ $categories = getAllCategoriesWithCountRecipes();
       <div class="col-sm-6 p-md-0">
         <div class="welcome-text">
           <h4>Hi, welcome back!</h4>
-          <span>Datatable</span>
         </div>
       </div>
       <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
