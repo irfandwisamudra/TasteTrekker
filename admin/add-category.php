494258c99876082cbe61a7a4961649dd0d3eb33a
irfandwisamudra <irfandwisamudra08@gmail.com>
2023-12-26 11:33:21 +0700

    Create add-category.php

---
+++ admin/add-category.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Tambah Kategori - TasteTrekker Admin";
+
+include "../includes/main_start.php";
+
+$categories = getAllCategoriesWithCountRecipes();
+?>
+
+
+
+<?php include "../includes/main_end.php"; ?>
