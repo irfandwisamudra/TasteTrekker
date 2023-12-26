ef53858e6f6ce46bd3d97dceef175a1d7f7eaaaa
irfandwisamudra <irfandwisamudra08@gmail.com>
2023-12-26 12:30:28 +0700

    Add comments

--- includes/main_end.php
+++ includes/main_end.php
@@ -5,18 +5,24 @@
     </div>
   <?php endif; ?>
 
+  <!-- Jquery -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <!-- Bootstrap -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
 
+  <!-- Required vendors -->
   <script src="<?= BASEURL ?>/assets/libs/vendor/global/global.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/js/bootstrap-select.min.js"></script>
   <script src="<?= BASEURL ?>/assets/js/custom.min.js"></script>
   <script src="<?= BASEURL ?>/assets/js/deznav-init.js"></script>
 
+  <!-- Datatable -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js"></script>
   <script src="<?= BASEURL ?>/assets/js/datatables.init.js"></script>
 
+  <!-- Jquery Validation -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.20.0/jquery.validate.min.js"></script>
+  <!-- Form validate init -->
   <script src="<?= BASEURL ?>/assets/js/jquery.validate-init.js"></script>
 </body>
 
