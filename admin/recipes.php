389bd919a18ec1ed2f6244879e0d4e924f55f79f
irfandwisamudra <irfandwisamudra08@gmail.com>
2023-12-27 21:09:36 +0700

    Create recipes.php

---
+++ admin/recipes.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Daftar Resep - TasteTrekker Admin";
+
+include "../includes/main_start.php";
+
+$menus = getAllRecipesWithNameMenu();
+?>
+
+
+
+<?php include "../includes/main_end.php"; ?>
