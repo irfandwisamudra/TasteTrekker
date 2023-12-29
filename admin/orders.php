79546e51ccc4336c2b18f0c574b6a86964dc76bd
irfandwisamudra <irfandwisamudra08@gmail.com>
2023-12-29 13:41:19 +0700

    Create orders.php

---
+++ admin/orders.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Daftar Pesanan - TasteTrekker Admin";
+
+include "../includes/main_start.php";
+
+$orders = getAllOrdersWithUsername();
+?>
+
+
+
+<?php include "../includes/main_end.php"; ?>
