765a3aa15cfb7d0823f0c868cf49c5ce78945e00
irfandwisamudra <irfandwisamudra08@gmail.com>
2023-12-29 15:22:29 +0700

    Add form order

--- admin/add-order.php
+++ admin/add-order.php
@@ -4,6 +4,66 @@ $title = "Tambah Pesanan - TasteTrekker Admin";
 include "../includes/main_start.php";
 ?>
 
-
+<div class="content-body">
+  <div class="container-fluid">
+    <div class="row page-titles mx-0">
+      <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="javascript:void(0)">Pesanan</a></li>
+          <li class="breadcrumb-item active"><a href="javascript:void(0)">Tambah Pesanan</a></li>
+        </ol>
+      </div>
+    </div>
+    <!-- row -->
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card">
+          <div class="card-header">
+            <h4 class="card-title">Tambah Pesanan</h4>
+          </div>
+          <div class="card-body">
+            <div class="form-validation">
+              <form class="form-valide-recipe" action="#" method="post">
+                <div class="row">
+                  <div class="col-xl-12">
+                    <div class="form-group row">
+                      <label class="col-lg-4 col-form-label" for="val-user">Username
+                        <span class="text-danger">*</span>
+                      </label>
+                      <div class="col-lg-6">
+                        <input type="text" class="form-control" id="val-user" name="val-user" placeholder="Pilih username..">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label class="col-lg-4 col-form-label" for="val-payment">Pembayaran
+                        <span class="text-danger">*</span>
+                      </label>
+                      <div class="col-lg-6">
+                        <input type="text" class="form-control" id="val-payment" name="val-payment" placeholder="Pilih Pembayaran..">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label class="col-lg-4 col-form-label" for="val-delivery">Pengiriman
+                        <span class="text-danger">*</span>
+                      </label>
+                      <div class="col-lg-6">
+                        <input type="text" class="form-control" id="val-delivery" name="val-delivery" placeholder="Pilih pengiriman..">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <div class="col-lg-8 ml-auto">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php include "../includes/main_end.php"; ?>
