1b7da48994c34a09a899d321c4ac5c4f66243cf6
irfandwisamudra <irfandwisamudra08@gmail.com>
2023-12-29 13:02:45 +0700

    Add form validate recipe

--- assets/js/jquery.validate-init.js
+++ assets/js/jquery.validate-init.js
@@ -36,6 +36,62 @@ jQuery(".form-valide-category").validate({
     },
 });
 
+jQuery(".form-valide-recipe").validate({
+    rules: {
+        "val-title": {
+            required: !0
+        },
+        "val-menu": {
+            required: !0
+        },
+        "val-desc": {
+            required: !0
+        },
+        "val-serving": {
+            required: !0
+        },
+        "val-timing": {
+            required: !0
+        },
+        "val-image": {
+            required: !0
+        }
+    },
+    messages: {
+        "val-title": {
+            required: "Silakan masukkan judul"
+        },
+        "val-menu": {
+            required: "Silahkan pilih menu"
+        },
+        "val-desc": {
+            required: "Silahkan masukkan deskripsi"
+        },
+        "val-serving": {
+            required: "Silahkan masukkan penyajian"
+        },
+        "val-timing": {
+            required: "Silahkan masukkan waktu"
+        },
+        "val-image": {
+            required: "Silahkan upload gambar"
+        }
+    },
+
+    ignore: [],
+    errorClass: "invalid-feedback animated fadeInUp",
+    errorElement: "div",
+    errorPlacement: function(e, a) {
+        jQuery(a).parents(".form-group > div").append(e)
+    },
+    highlight: function(e) {
+        jQuery(e).closest(".form-group").removeClass("is-invalid").addClass("is-invalid")
+    },
+    success: function(e) {
+        jQuery(e).closest(".form-group").removeClass("is-invalid"), jQuery(e).remove()
+    },
+});
+
 jQuery(".form-valide-menu").validate({
     rules: {
         "val-name": {
