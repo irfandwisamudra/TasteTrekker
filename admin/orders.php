729c44a02b851a051ce554ab5b0328d4114c3241
irfandwisamudra <irfandwisamudra08@gmail.com>
2023-12-29 15:29:20 +0700

    Delete welcome text

--- admin/orders.php
+++ admin/orders.php
@@ -9,11 +9,6 @@ $orders = getAllOrdersWithUsername();
 <div class="content-body">
   <div class="container-fluid">
     <div class="row page-titles mx-0">
-      <div class="col-sm-6 p-md-0">
-        <div class="welcome-text">
-          <h4>Hi, welcome back!</h4>
-        </div>
-      </div>
       <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="javascript:void(0)">Pesanan</a></li>
