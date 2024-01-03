ae750ac3ddb41834885657dbc0f2361c314fd5ed
irfandwisamudra <irfandwisamudra08@gmail.com>
2024-01-03 21:05:24 +0700

    Change responsive footer

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,12 +1,12 @@
 <footer class="footer border-top">
   <div class="container">
     <div class="row row-cols-1 row-cols-md-5 py-5">
-      <div class="col col-md-5 me-md-auto mb-3">
+      <div class="col col-lg-4 col-md-6 mb-3">
         <h5>About Us</h5>
         <p class="text-body-secondary">Short description about your website or company.</p>
       </div>
 
-      <div class="col col-md-2 mb-3">
+      <div class="col col-lg-2 col-md-6 mb-3">
         <h5>Quick Links</h5>
         <ul class="nav flex-column">
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
@@ -17,7 +17,7 @@
         </ul>
       </div>
 
-      <div class="col col-md-2 mb-3">
+      <div class="col col-lg-2 col-md-6 mb-3">
         <h5>Pages</h5>
         <ul class="nav flex-column">
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Privacy Policy</a></li>
@@ -26,7 +26,7 @@
         </ul>
       </div>
 
-      <div class="col col-md-3 mb-3">
+      <div class="col col-lg-4 col-md-6 mb-3">
         <h5>Contact Us</h5>
         <p class="text-body-secondary">Address: Jalan Trunojoyo, Bangkalan</p>
         <p class="text-body-secondary">Phone: +62 852 3655 3899</p>
@@ -38,7 +38,7 @@
   <div class="copyright border-top">
     <div class="container">
       <div class="col col-md-12 d-flex flex-column flex-sm-row justify-content-between align-items-center py-4">
-        <p class="text-light">&copy; 2023 TasteTrekker. All rights reserved.</p>
+        <p class="text-light my-auto">&copy; 2023 TasteTrekker. All rights reserved.</p>
         <ul class="list-unstyled d-flex mb-0">
           <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-twitter"></i></a></li>
           <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-instagram"></i></a></li>
