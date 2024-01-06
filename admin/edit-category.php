9788a3ddf3092d20648749f1d0917b08fb73d234
irfandwisamudra <irfandwisamudra08@gmail.com>
2024-01-06 22:33:42 +0700

    Add main start and main end

---
+++ admin/edit-category.php
@@ -0,0 +1,14 @@
+<?php
+$title = "Edit Kategori - TasteTrekker Admin";
+
+require_once "../includes/main_start.php";
+
+if (!isset($_SESSION["login"]) || $_SESSION["login"] != true || $_SESSION["level"] != 1) {
+  header("Location: ../index.php");
+  exit;
+}
+?>
+
+
+
+<?php require_once "../includes/main_end.php"; ?>
